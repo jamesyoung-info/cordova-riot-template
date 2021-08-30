632767e0025dbd2ecd7ab7de635c71d0854d7de2
jamesyoung-info <me@jamesyoung.info>
2021-08-30 12:48:34 +1000

    Try to add example TODO app for Cordova

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { component } from "riot";
-import AppComponent from "./components/app.riot";
-
 document.addEventListener("deviceready", () => {
-    component(AppComponent)(document.querySelector("app") as HTMLElement);
+    console.log('Ready')
 }, false);
